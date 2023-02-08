8d80b93add3876929c32375b645610d6dd374ce1
wpower12 <willpowe@gmail.com>
2023-02-08 13:44:26 -0500

    removing pg from webpack for now. adding saltcalc.

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,17 @@ const path = require('path');
 module.exports = {
 	mode: 'development',
 	entry: {
+		saltcalc:   './src/js/saltcalc/main.js',
 		strums:     './src/js/strums.js',
 		beebc:      './src/js/beebc/main.js',
 		forces:     './src/js/forces/main.js',
 		traffic:    './src/js/traffic/traffic.js',
 		tribes:     './src/js/tribes/main.js',
 		miniclient: './src/js/minimog/client/client.js',
-		pg_global: './src/js/procgen/viewglobal.js'
+		// pg_global:  './src/js/procgen/viewglobal.js'
 	},
 	output: {
 	    path: path.resolve(__dirname, 'public/javascripts'),
 	    filename: '[name].bundle.js'
     },
-   // Man I need to figure out why this was needed for the socket.io stuff. 
-   //  module: {
-	  //   rules: [
-	  //     {
-	  //       test: /\.m?js$/,
-	  //       use: {
-	  //         loader: "babel-loader",
-	  //         options: {
-	  //           presets: ["@babel/preset-env"], // ensure compatibility with older browsers
-	  //           plugins: ["@babel/plugin-transform-object-assign"], // ensure compatibility with IE 11
-	  //         },
-	  //       },
-	  //     },
-	  //     {
-	  //       test: /\.js$/,
-	  //       loader: "webpack-remove-debug", // remove "debug" package
-	  //     },
-	  //   ],
-  	// },
 };
